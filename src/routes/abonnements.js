5839056cc9b5aed903d44f4ee3b35ae2e838c9ff
Mistergael <anguissagael6@gmail.com>
2026-04-18 16:19:51 +0100

    Correction ERR_REQUIRE_ESM : utilisation de crypto.randomUUID

--- src/routes/abonnements.js
+++ src/routes/abonnements.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const { protect, structureOnly } = require('../middleware/auth');
-const { v4: uuidv4 } = require('uuid');
+const crypto = require('crypto');
+const uuidv4 = () => crypto.randomUUID();
 
 const prisma = new PrismaClient();
 
